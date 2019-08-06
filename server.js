45b2bada6e5814f04235769147fac149df992cd9
purple-giraffey <dmarina0706@yahoo.com>
2019-08-06 19:50:24 +0200

    favicon added, changes in server.js

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const path = require('path');
+const https = require('https');
 
 app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname + '/static', { dotfiles: 'allow' }));
 app.use("/styles", express.static(__dirname));
 app.use("/scripts", express.static(__dirname + '/scripts'));
+app.use('/favicon.ico', express.static('images/favicon.ico'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/bouncingBalls.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/purplegiraffe.dev/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/purplegiraffe.dev/cert.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/purplegiraffe.dev/chain.pem')
+}, app).listen(443, () => {
+  console.log('Listening...')
+});
+
+//app.listen(process.env.PORT || 80);
